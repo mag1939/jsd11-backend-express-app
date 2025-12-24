c1dc0867983e2c71e15c643af3a6b29905d8ea2e
mag1939 <songsak.thawaro@gmail.com>
2025-12-24 15:22:18 +0700

    refactored user routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { router as apiRoutes } from "./routes/index.js";
+import { testAPI } from "./modules/users/users.controller.js";
 
+// setup Express.js structure
 export const app = express();
 
 app.use(express.json());
+
+app.use("/", testAPI);
+app.use("/api", apiRoutes);
---
+++ src/mock-db/users.js
@@ -0,0 +1,5 @@
+export const users = [
+    {id: "1", name: "Mag", email: "mag@example.com"},
+    {id: "2", name: "doggy", email: "doggy@example.com"},
+    {id: "3", name: "Zipzap", email: "Zipzap@example.com"},
+]
