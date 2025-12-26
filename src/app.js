c82f7385fdb6f1012c05a8b7d77f20db884f3eef
mag1939 <songsak.thawaro@gmail.com>
2025-12-26 17:29:52 +0700

    install CORS for allow which sites can communicate with our app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { router as apiRoutes } from "./routes/index.js";
+import cors from "cors";
 
 
 // setup Express.js structure
 export const app = express();
 
+// enable CORS policy
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+    ]
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", apiRoutes);
